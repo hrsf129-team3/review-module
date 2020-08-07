1b3a71c81cb1fd27af8b9406a1fa9eb3df0c0810
Reed Brooks <reedb36@gmail.com>
2020-08-07 11:47:47 -0700

    Begin work on Review React components and API

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/production');
+
 module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
   module: {
-    rules: [
+    loaders: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        test: /\.jsx?/,
+        include: SRC_DIR,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
         }
       }
     ]
