b939bb1a76316d78f4953bab087508537d0c4cb7
Reed Brooks <reedb36@gmail.com>
2020-08-08 12:55:14 -0700

    Webpack configuration

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,14 @@
-console.log("Test...");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class ReviewContainer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (<h1>Test.</h1>);
+  }
+}
+
+ReactDOM.render(<ReviewContainer />, document.getElementById('review-app'));
