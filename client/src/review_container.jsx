303750c3c6e314dea12d91d9d2a21017bd85ce14
Reed Brooks <reedb36@gmail.com>
2020-08-11 19:40:26 -0700

    Add arrows to pagination

--- client/src/review_container.jsx
+++ client/src/review_container.jsx
@@ -98,6 +98,17 @@ class ReviewContainer extends React.Component {
   }
 }
 
+
+//arrow SVGs for Pagination
+const rightArrow = (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" height="18" width="18">
+<path d="M17.3 12.7l.7-.7-.7-.7-4-4c-.4-.4-1-.4-1.4 0s-.4 1 0 1.4l2.3 2.3H7c-.6 0-1 .4-1 1s.4 1 1 1h7.2l-2.3 2.3c-.2.2-.3.4-.3.7 0 .6.4 1 1 1 .3 0 .5-.1.7-.3l4-4z"></path>
+</svg>);
+
+const leftArrow = (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true" focusable="false" height="18" width="18">
+<path d="M6.7 11.3L6 12l.7.7 4 4c.4.4 1 .4 1.4 0 .4-.4.4-1 0-1.4L9.8 13H17c.6 0 1-.4 1-1s-.4-1-1-1H9.8l2.3-2.3c.2-.2.3-.4.3-.7 0-.6-.4-1-1-1-.3 0-.5.1-.7.3l-4 4z"></path>
+</svg>);
+
+
 //page counter class for Review container module
 //Etsy pagination looks like the following:
 // (<-) (pages) (->)
@@ -115,7 +126,7 @@ class ReviewPagination extends React.Component {
 
 
   render() {
-    return (<div>Page {this.props.currentPage} of {this.props.maxPage}</div>);
+    return (<div>{leftArrow}Page {this.props.currentPage} of {this.props.maxPage}{rightArrow}</div>);
   }
 }
 
