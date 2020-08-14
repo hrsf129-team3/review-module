a7312f9d592ec3e356d1f4a09cd1aac48159fee6
Reed Brooks <reedb36@gmail.com>
2020-08-13 22:22:07 -0700

    Create CSS subfolder and style file

--- webpack.config.js
+++ webpack.config.js
@@ -22,9 +22,13 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true
+            }
+          }
         ]
-      }
-    ]
   }
 };
