a5b60d69494f0cc06194e07998d955ec927f8b7b
Reed Brooks <reedb36@gmail.com>
2020-08-23 00:28:58 -0700

    Implement serving compressed JS file in component server

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var db = require('../database');
+const path = require('path');
+const expressStaticGzip = require("express-static-gzip");
+const prodPath = path.join(__dirname, '../client/production');
 
 var app = express();
 
 app.use( bodyParser.json() );
 
 //root directory declaration
-app.use(express.static(__dirname + '/../client/production'));
+//app.use(express.static(__dirname + '/../client/production'));
+app.use(expressStaticGzip(prodPath, { enableBrotli: true, orderPreference: ['br'] }));
 
 //helper function declarations
 
